ab950fa8eb8d25423786983aab72486a1e1e3984
Simon Torres <storres@ctio.noao.edu>
2019-03-12 11:17:58 -0300

    created api routes and added several dependencies into app.js as well as installed some other packages

--- web_app/app.js
+++ web_app/app.js
@@ -1,14 +1,28 @@
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var morgan = require('morgan');
+var mongoose = require('mongoose');
+var passport = require('passport');
+var bodyParser = require('body-parser');
+var favicon = require('serve-favicon');
+var config = require('./config/database');
+
+mongoose.Promise = require('bluebird');
+mongoose.connect(config.database, { promiseLibrary: require('bluebird')})
+    .then(() => console.log('Connection to database succesful'))
+    .catch((err) => console.error(err));
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var apiRouter = require('./routes/api');
 
 var app = express();
 
-app.use(logger('dev'));
+app.use(morgan('dev'));
+app.use(passport.initialize());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({'extended':'false'}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -16,5 +30,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/api', apiRouter);
+
 
 module.exports = app;
